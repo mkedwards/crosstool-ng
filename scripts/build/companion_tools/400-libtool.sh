09c9cac3e932123640e33955609e51a05d9d5321
Michael K. Edwards <michaedw@cisco.com>
2011-07-15 11:46:19 -0700

    build libtool for target

--- scripts/build/companion_tools/400-libtool.sh
+++ scripts/build/companion_tools/400-libtool.sh
@@ -25,4 +25,35 @@ do_companion_tools_libtool_build() {
     CT_DoExecLog ALL make install
     CT_Popd
     CT_EndStep
+
+    CT_DoStep EXTRA "Installing libtool for the target"
+    mkdir -p "${CT_BUILD_DIR}/build-libtool-for-target"
+    CT_Pushd "${CT_BUILD_DIR}/build-libtool-for-target"
+
+    CT_DoLog EXTRA "Configuring libtool for the target"
+    cp ../../config.cache .
+    CT_DoExecLog CFG                                            \
+    CC="${CT_TARGET}-gcc"                                       \
+    CFLAGS="-g -Os -fPIC -DPIC"                                 \
+    ./configure                                                 \
+        --build=${CT_BUILD}                                     \
+        --host=${CT_TARGET}                                     \
+        --target=${CT_TARGET}                                   \
+        --cache-file="$(pwd)/config.cache"                      \
+        --sysconfdir=/etc                                       \
+        --localstatedir=/var                                    \
+        --mandir=/usr/share/man                                 \
+        --infodir=/usr/share/info                               \
+        --prefix=/usr                                           \
+        --enable-shared                                         \
+        --enable-static
+
+    CT_DoLog EXTRA "Building libtool for the target"
+    CT_DoExecLog ALL make
+
+    CT_DoLog EXTRA "Installing libtool for the target"
+    CT_DoExecLog ALL make DESTDIR="${CT_SYSROOT_DIR}" install
+
+    CT_Popd
+    CT_EndStep
 }
