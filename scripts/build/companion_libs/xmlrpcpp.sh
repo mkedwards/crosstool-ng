09c50d823d8d6485349558298026535991f61df3
michaedw in build chroot <build@ctbu-bld4.cisco.com>
2011-07-24 23:18:27 +0000

    fix xmlrpcpp download, add missing dummy-opengl-es2 Makefile, remove cruft

--- scripts/build/companion_libs/xmlrpcpp.sh
+++ scripts/build/companion_libs/xmlrpcpp.sh
@@ -8,14 +8,15 @@ do_xmlrpcpp_target() { :; }
 if [ "${CT_XMLRPCPP}" = "y" -o "${CT_XMLRPCPP_TARGET}" = "y" ]; then
 
 do_xmlrpcpp_get() {
-    CT_GetFile "xmlrpcpp-${CT_XMLRPCPP_VERSION}" .tar.gz    \
-               "http://mesh.dl.sourceforge.net/sourceforge/xmlrpcpp/xmlrpc++/Version ${CT_XMLRPCPP_VERSION}"
+    CT_GetFile "xmlrpc++${CT_XMLRPCPP_VERSION}" .tar.gz    \
+               "http://mesh.dl.sourceforge.net/sourceforge/xmlrpcpp/xmlrpc%2B%2B/Version%20${CT_XMLRPCPP_VERSION}"
     # Downloading from sourceforge may leave garbage, cleanup
     CT_DoExecLog ALL rm -f "${CT_TARBALLS_DIR}/showfiles.php"*
 }
 
 do_xmlrpcpp_extract() {
-    CT_Extract "xmlrpcpp-${CT_XMLRPCPP_VERSION}"
+    CT_Extract "xmlrpc++${CT_XMLRPCPP_VERSION}"
+    mv "${CT_SRC_DIR}/xmlrpc++${CT_XMLRPCPP_VERSION}" "${CT_SRC_DIR}/xmlrpcpp-${CT_XMLRPCPP_VERSION}"
     CT_Patch "xmlrpcpp" "${CT_XMLRPCPP_VERSION}"
 }
 
@@ -25,7 +26,7 @@ do_xmlrpcpp() {
     CT_DoStep INFO "Installing xmlrpcpp"
     rm -rf "${CT_BUILD_DIR}/build-xmlrpcpp"
     cp -a "${CT_SRC_DIR}/xmlrpcpp-${CT_XMLRPCPP_VERSION}" "${CT_BUILD_DIR}/build-xmlrpcpp"
-    CT_Pushd "${CT_BUILD_DIR}/build-xmlrpcpp/xmlrpc++"
+    CT_Pushd "${CT_BUILD_DIR}/build-xmlrpcpp"
 
     CT_DoLog EXTRA "Building xmlrpcpp"
     CT_DoExecLog ALL make ${JOBSFLAGS}                          \
@@ -51,7 +52,7 @@ do_xmlrpcpp_target() {
     CT_DoStep INFO "Installing xmlrpcpp for the target"
     rm -rf "${CT_BUILD_DIR}/build-xmlrpcpp-for-target"
     cp -a "${CT_SRC_DIR}/xmlrpcpp-${CT_XMLRPCPP_VERSION}" "${CT_BUILD_DIR}/build-xmlrpcpp-for-target"
-    CT_Pushd "${CT_BUILD_DIR}/build-xmlrpcpp-for-target/xmlrpc++"
+    CT_Pushd "${CT_BUILD_DIR}/build-xmlrpcpp-for-target"
 
     CT_DoLog EXTRA "Building xmlrpcpp"
     CT_DoExecLog ALL make ${JOBSFLAGS}                          \
